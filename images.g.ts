7defa9440f749dba6be88324a446a26ba0bd567f
Raghavdevil <114174851+Raghavdevil@users.noreply.github.com>
2023-02-12 00:32:41 +0530

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "Boy":return hex`0078000408020400001c00010a006400f401640000040000000000000000000000000005000004180004000800012a0c001000012a24002800011d2c003000011d01001c000f05001202c102c2010004050028000000640028000300000600000418000c00100001271400180001272c003000011934003800011904001c00100500640000041e000004000000000000000000000000000a0000040c001c002000012024002800012007001c00020a006400f40164000004000000000000000000000000000000000312001400180001241c00200001243c0040000119`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
